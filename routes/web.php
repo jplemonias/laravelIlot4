4780c8579121f7af6aef17de73f4c7212184dac4
Martin BRANDEL <martin.brandel@Digital-Grenoble.local>
2022-03-09 16:08:19 +0100

    commit products page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ProductsController::class,'show']);
